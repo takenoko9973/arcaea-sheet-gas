7a8499b3dd198c1e58b477167461c16d60fcc77e
taketomo3141 <sapphire3141@gmail.com>
2024-06-29 19:35:34 +0000

    統計データ更新部分を分離

---
+++ src/app/dailyStatisticsUpdate.ts
@@ -0,0 +1,35 @@
+import { Difficulty } from "../types/difficulty";
+import { DailyArcaeaData, DailyRepositorySheet } from "../class/sheet/dailyRepositorySheet";
+import { SongScoreSheet } from "../class/sheet/songScoreSheet";
+import { GradeData } from "../class/gradeData";
+import { ScoreData } from "../class/scoreData";
+
+export function updateDailyStatistics() {
+    console.log("Update daily statistics");
+
+    const statisticsDifficulties: Difficulty[] = [
+        Difficulty.Future,
+        Difficulty.Beyond,
+        Difficulty.Eternal,
+    ];
+
+    const dailySheet = DailyRepositorySheet.instance;
+    const songScoreSheet = SongScoreSheet.instance;
+
+    const today = new Date();
+    const bestPotential = songScoreSheet.getBestPotential();
+
+    let gradeData = new GradeData();
+    const scoreData = [];
+    for (const difficulty of statisticsDifficulties) {
+        const grade = songScoreSheet.getGrades(difficulty);
+        gradeData = gradeData.plus(grade);
+
+        const maximumScore = songScoreSheet.getMaximumSumScore(difficulty);
+        const score = songScoreSheet.getSumScore(difficulty);
+        scoreData.push(new ScoreData(score, maximumScore - score));
+    }
+
+    const dailyData = new DailyArcaeaData(today, bestPotential, gradeData, scoreData);
+    dailySheet.addData(dailyData);
+}

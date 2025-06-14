565adf2ee25452e858fff8313c12d98cf909d642
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2025-06-14 14:42:52 +0000

    songFactory追加

--- src/app/services/wikiDataFetcherService.ts
+++ src/app/services/wikiDataFetcherService.ts
@@ -6,7 +6,7 @@ import { IFetchArcaeaWiki } from "../../@types/fetch-arcaea-wiki";
 declare const FetchArcaeaWiki: IFetchArcaeaWiki;
 
 // Wikiから取得するデータの型を定義
-type WikiSongDetails = { composer: string; pack: string; version: string; notes: number };
+export type WikiSongDetails = { composer: string; pack: string; version: string; notes: number };
 
 export class WikiDataFetcherService {
     /**
@@ -14,7 +14,7 @@ export class WikiDataFetcherService {
      * @param dto 楽曲情報DTO
      * @returns 取得した詳細データ
      */
-    public static fetchDetails(dto: SongCollectionDto): WikiSongDetails {
+    static fetchDetails(dto: SongCollectionDto): WikiSongDetails {
         // wikiから、追加のデータを取得
         const songDataFromWiki = FetchArcaeaWiki.createSongData(dto.urlName);
         Utilities.sleep(1500); // サーバーに負荷をかけないようにする
